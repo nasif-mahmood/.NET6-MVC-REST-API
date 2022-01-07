dcdfda092c1ae3ff9ca21959d60c716c261cc8d7
Nasif Mahmood <nasifmahmood@Nasifs-MBP.lan>
2022-01-06 18:29:44 -0600

    first commit

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,37 @@
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    
+    [Route("api/commands")] // How you get to resources and API endpoints
+    [ApiController] // Gives out of box behaviors for controller
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repository;
+
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
+        //private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        //GET api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+
+            return Ok(commandItems);
+        }
+
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
+    }
+}
