92cfcccb3ca599c9a6fffcc1f202bf26ec08609b
nasif <nasif.mahmood456@gmail.com>
2022-01-08 16:30:08 -0600

    Added database connection

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using Commander.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer
+    (builder.Configuration.GetConnectionString("CommanderConnection")));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+//builder.Services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+builder.Services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
 
 var app = builder.Build();
 
